b7ae8a767ed8563dd1cbd034f4e31c26cd0ea5ea
SecteurA <112476252+SecteurA@users.noreply.github.com>
2024-10-29 02:47:28 +0100

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -45,7 +45,7 @@ const features = [
 
 export default function Features() {
   return (
-    <section className="py-24">
+    <section id="features" className="py-24">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
